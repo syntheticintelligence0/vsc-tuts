943cfe027ba591eb88f7c6985dc6e49d761b43ce
synthetic intelligence <syntheticintelligence0@gmail.com>
2018-12-18 18:45:20 +0530

    thi is a automated commit

---
+++ automated commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git init
+git add .
+
+git status
+
+git commit -m "thi is a automated commit"
+
+git status
+
+echo "key to exit"
+read value
---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git init
+
+
+git push
+
+git status
+
+echo "press any key to exit"
+read value
